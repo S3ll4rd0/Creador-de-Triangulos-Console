467a5e70d5198bc73c69abf8bf60bae146e4b29e
Antonio Nicolás Salmerón Rubio <54323367+S3ll4rd0@users.noreply.github.com>
2022-07-03 22:37:40 +0200

    Add files to repository via upload
    Añado los ficheros del proyecto al repositorio

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Triangulo
+{
+    public class Triangulo
+    {
+        public static void Main(String [] args)
+        {
+            double ladoTriangulo = 0;
+            double alturaTriangulo = 0; 
+            double baseTriangulo = 0;
+
+            Console.WriteLine("Introduzca el lado del triángulo.");
+            ladoTriangulo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduzca la altura del triángulo.");
+            alturaTriangulo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduzca la base del triángulo.");
+            baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            if (ladoTriangulo == alturaTriangulo)
+            {
+                if (ladoTriangulo == baseTriangulo)
+                {
+                    Console.WriteLine("El triángulo es equilatero");
+                }
+            }
+            if(ladoTriangulo > baseTriangulo)
+            {
+                if(ladoTriangulo > alturaTriangulo)
+                {
+                    Console.WriteLine("El lado mayor es el Lado del triángulo: " + ladoTriangulo);
+                }
+                else
+                {
+                    Console.WriteLine("El lado mayor es la altura del triángulo: " + alturaTriangulo);
+                }
+            }
+            else
+            {
+                if (baseTriangulo > alturaTriangulo)
+                {
+                    Console.WriteLine("El lado mayor es la base del triángulo: " + baseTriangulo);
+                }
+                else
+                {
+                    Console.WriteLine("El lado mayor es la altura del triángulo: " + alturaTriangulo);
+                }
+            }
+
+            Console.WriteLine("El area del triángulo es: " + ((baseTriangulo * alturaTriangulo) / 2));
+        }
+    }
+}
